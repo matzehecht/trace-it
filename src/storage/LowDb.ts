1dd9e96a4021abd3d959ce0071a71f0739fbf681
Matthias Hecht <matthias@hecht.space>
2020-09-21 22:55:06 +0200

    refactor(storage): prepares the storage for analyze

--- src/storage/LowDb.ts
+++ src/storage/LowDb.ts
@@ -4,7 +4,7 @@ import { CollectionChain } from 'lodash';
 import { nanoid } from 'nanoid';
 
 import { InitStorageOptions, TransactionData } from '../models';
-import { Adapter as StorageAdapter } from './models';
+import { Adapter as StorageAdapter, TransactionInstance } from './models';
 
 interface LowDbDatabase {
   transactions: LowDbTransactionCollection;
@@ -93,5 +93,33 @@ export class LowDbAdapter implements StorageAdapter {
     } else {
       (await this.db).get('transactionData').find({ syntacticId }).assign(x).write();
     }
+  }
+
+  public async getChildren(semanticId: string[]): Promise<string[][]> {
+    const pathToTransaction = semanticId.length === 0 ? '' : `.${semanticId.join('.children.')}.children`;
+
+    return (await this.db)
+      .get(`transactions${pathToTransaction}`)
+      .keys()
+      .value()
+      .map((id) => [...semanticId, id]);
+  }
+
+  public async getInstancesTimings(semanticId: string[]): Promise<(number | undefined)[]> {
+    const pathToTransaction = semanticId.join('.children.');
+
+    return (((await this.db).get(`transactions.${pathToTransaction}.instances`) as unknown) as CollectionChain<LowDbTransactionInstance>)
+      .map('timing').value();
+  }
+
+  public async getInstances(semanticId: string[]): Promise<TransactionInstance[]> {
+    const pathToTransaction = semanticId.join('.children.');
+
+    return (((await this.db).get(`transactions.${pathToTransaction}.instances`) as unknown) as CollectionChain<LowDbTransactionInstance>)
+      .value();
+  }
+
+  public async getTransactionData(syntacticId: string): Promise<TransactionData> {
+    return (await this.db).get('transactionData').find( { syntacticId }).value();
   }
 }
